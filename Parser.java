1942946373f9918f9cf8b3813ecaca3afb3f05ac
Sithale2 <93401264+Sithale2@users.noreply.github.com>
2022-06-01 13:41:54 -0700

    Add files via upload

--- Parser.java
+++ Parser.java
@@ -3,8 +3,14 @@ package cs350s22.component.ui.parser;
 public class Parser
 {
 	
+	A_ParserHelper helper;
+	String command;
+	
 	public Parser(A_ParserHelper parserHelper, String commandText)
 	{
+		this.helper = parserHelper;
+		this.command = commandText;
+		
 		
 	}
 	
