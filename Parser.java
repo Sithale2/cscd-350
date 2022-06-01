9b578d7eb1dc469f175e10344ca0dc9b2cd989c6
Sithale2 <93401264+Sithale2@users.noreply.github.com>
2022-06-01 13:30:18 -0700

    Add files via upload

--- Parser.java
+++ Parser.java
@@ -8,17 +8,8 @@ import cs350s22.support.Identifier;
 
 public class Parser
 {
-
-	public ParserTokenManager token_source;
-	public Token token;
-	public Token jj_nt;
 	
 	public Parser(A_ParserHelper parserHelper, String commandText)
-	{
-		
-	}
-	
-	public Parser(InputStream stream)
 	{
 		
 	}
@@ -27,104 +18,10 @@ public class Parser
 	{
 		
 	}
-	
-	public Parser(Reader stream)
-	{
-		
-	}
-	
-	public Parser(ParserTokenManager tm)
-	{
-		
-	}
-	
-	public final void disable_tracing()
-	{
-		
-	}
-	
-	public final void enable_tracing()
-	{
-		
-	}
-	
-	public final String filename() throws ParseException
-	{
-		return null;
-    }
-	
-	public ParseException generateParseException()
-	{
-		return null;
-	}
-	
-	public final Token getNextToken()
-	{
-		return null;
-	}
-	
-	public final Token getToken(int index)
-	{
-		return null;
-	}
-	
-	public final List<Identifier> groups() throws ParseException
-	{
-		return null;
-	}
-	
-	public final Identifier identifier() throws ParseException
-	{
-		return null;
-	}
-	
-	public final List<Identifier> idList() throws ParseException
-	{
-		return null;
-	}
-	
-	public final List<Identifier> ids() throws ParseException
-	{
-		return null;
-	}
-	
-	public final int integer() throws ParseException
-	{
-		return 0;
-	}
-	
-	public final double number() throws ParseException
-	{
-		return 0;
-	}
-	
-	public final void parse() throws ParseException, IOException
-	{
-		
-	}
-	
-	public final double real() throws ParseException
-	{
-		return 0;
-	}
-	
-	public void ReInit(InputStream stream)
-	{
-		
-	}
-	
-	public void ReInit(InputStream stream, String encoding)
-	{
-		
-	}
 
-	public void ReInit(Reader stream)
+	public void parse()
 	{
 		
 	}
 	
-	public void ReInit(ParserTokenManager tm)
-	{
-		
-	}
 }
