2f61778789f94c1314c9f3015ef3f7a3d45aa0f1
Sithale2 <93401264+Sithale2@users.noreply.github.com>
2022-06-01 13:33:07 -0700

    Add files via upload

--- Parser.java
+++ Parser.java
@@ -1,10 +1,4 @@
 package cs350s22.component.ui.parser;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.util.List;
-
-import cs350s22.support.Identifier;
 
 public class Parser
 {
@@ -14,11 +8,6 @@ public class Parser
 		
 	}
 	
-	public Parser(InputStream stream, String encoding)
-	{
-		
-	}
-
 	public void parse()
 	{
 		
